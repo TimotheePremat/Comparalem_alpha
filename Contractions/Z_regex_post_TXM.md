34ca9f4283e8f07e8aba22aa0f8dde358d5f1c8c
TimotheePremat <timothee.premat@gmail.com>
2023-06-15 09:53:45 +0200

    Cleaning the repo
    Removing all non necessary files for a minimal working repository.

--- Contractions/Z_regex_post_TXM.md
+++
@@ -1,8 +0,0 @@
-([A-Za-z0-9_]*(sg|pl))_
-$1;
-
-(PROCON|000|PRE|ADV|PRO_invar|ERROR|ADJ_card|CON_[a-z]*)_
-$1;
-
-_([a-z]*;[0-9])
-;$1
--- NCA_schwa_interne/A6_chi-2_save.R
+++
@@ -1,9 +0,0 @@
-#Script to save regressions + Chi-2 plot.
-
-ggsave(p_POS_reg_chi,
-	      path = "Graphs",
-							filename = paste(my_var,"Chi-2.pdf"),
-							device=cairo_pdf,
-							width = 9,
-						 height = 4,
-						 units = "in")
